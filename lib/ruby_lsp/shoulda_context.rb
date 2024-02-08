463ebe32eb09b1a2800264fab86dc3efc6529e54
domingo2000 <dedwards@buk.cl>
2024-02-07 23:01:12 -0300

    feat: added installation instructions and fixed console

---
+++ lib/ruby_lsp/shoulda_context.rb
@@ -0,0 +1,11 @@
+# typed: strict
+# frozen_string_literal: true
+
+require_relative "shoulda_context/version"
+
+module RubyLsp
+  module ShouldaContext
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
